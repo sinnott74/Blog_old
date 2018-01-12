4437b347e9be8887348c47c9e06100ce3ca6fc51
Daniel Sinnott <danielsi@ie.ibm.com>
2018-01-12 15:54:22 +0000

    Fixing App's handling of service worker toast message

--- webclient/src/components/App/index.jsx
+++ webclient/src/components/App/index.jsx
@@ -6,7 +6,6 @@ import OptionsMenu from '../OptionsMenu';
 import Toast from '../Toast';
 import Spinner from '../Spinner';
 import Loadable from 'react-loadable';
-import { showToast } from '../../actions/toast';
 import './style.css'
 
 // import IndexPage from '../../pages/IndexPage';
@@ -39,7 +38,7 @@ const LoadableCodeListPage = Loadable({
 })
 
 
-export default class App extends React.Component {
+class App extends React.Component {
 
   constructor() {
     super();
@@ -66,6 +65,7 @@ export default class App extends React.Component {
   }
 
   _registerServiceWorker() {
+    let _self = this;
     if (!('serviceWorker' in navigator)) {
       // Service worker is not supported on this platform
       return;
@@ -76,6 +76,14 @@ export default class App extends React.Component {
     }).then(function(registration) {
       console.log('Service worker is registered.');
 
+      var isUpdate = false;
+
+      // If this fires we should check if there's a new Service Worker
+      // waiting to be activated. If so, ask the user to force refresh.
+      if (registration.active) {
+        isUpdate = true;
+      }
+
       registration.onupdatefound = function(updateEvent) {
         console.log('A new Service Worker version has been found...');
 
@@ -84,14 +92,24 @@ export default class App extends React.Component {
         // show a notification to the user.
         registration.installing.onstatechange = function(event) {
           if (this.state === 'installed') {
-            var message = (registration.active ? 'App updated. Restart for the new version.' : 'App ready for offline use.');
-            showToast(message);
+            var message = (isUpdate ? 'App updated. Restart for the new version.' : 'App ready for offline use.');
+            _self.props.handleToastMessage(message);
           }
         };
       };
+      console.log('After onupdatefound function set');
     })
     .catch(function(err) {
       console.log('Unable to register service worker.', err);
     });
   }
 }
+
+import { connect } from "react-redux";
+import { showToast } from '../../actions/toast';
+
+const mapDispatchToProps = {
+  handleToastMessage: showToast
+}
+
+export default connect(null, mapDispatchToProps)(App);
