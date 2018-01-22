f606774d19db6babaa8a19a5c4121e71327f57e2
Daniel Sinnott <danielsi@ie.ibm.com>
2018-01-22 14:09:25 +0000

    All className to input Icon props

--- webclient/src/components/Icon/index.js
+++ webclient/src/components/Icon/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Functional Component
 const Icon = (props) => {
-  return  <img src={props.img} alt={props.alt} title={props.alt}></img>
+  return  <img src={props.img} alt={props.alt} title={props.alt} className={props.className}></img>
 }
 
 export default Icon;
