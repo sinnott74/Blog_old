97a8b7c83fc23b97acd1567b6de25832e73c27e1
Daniel Sinnott <danielsi@ie.ibm.com>
2018-01-25 21:50:04 +0000

    Add toast duration

--- webclient/src/actions/toast.js
+++ webclient/src/actions/toast.js
@@ -1,15 +1,24 @@
 export const SHOW_TOAST = 'SHOW_TOAST';
 export const HIDE_TOAST = 'HIDE_TOAST';
 
-export function showToast(message){
-  return {
-    type: SHOW_TOAST,
-    message
+const defaultDuration = 3000;
+export function showToast(message, duration){
+  let toastDuration = duration || defaultDuration;
+  return function(dispatch) {
+    dispatch(displayToastMessage(message));
+    setTimeout(() => dispatch(hideToast()), toastDuration);
   }
 }
 
 export function hideToast(){
   return {
     type: HIDE_TOAST
+  }
+}
+
+function displayToastMessage(message) {
+  return {
+    type: SHOW_TOAST,
+    message
   }
 }
