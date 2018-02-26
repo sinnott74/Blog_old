ce89dd29ec26bcc5099cc9cad7d1414c0f1dd6ef
Daniel Sinnott <sinnott74@hotmail.com>
2018-02-26 15:59:02 +0000

    Add jsDoc

--- webclient/src/core/components/Routes/Routes.jsx
+++ webclient/src/core/components/Routes/Routes.jsx
@@ -4,6 +4,10 @@ import Loadable from 'react-loadable';
 import Spinner from 'core/components/Spinner';
 import AuthernticatedRoute from 'core/components/AuthenticatedRoute';
 
+/**
+ * Import all route pages
+ * @param {Array} Array of require contexts which map to a route config file
+ */
 function importAll(r) {
   return r.keys().map((item) => {
     return r(item).default;
@@ -11,28 +15,30 @@ function importAll(r) {
 }
 const pages = importAll(require.context('./', true, /.+\/pages\/pages.js/));
 
+/**
+ * Combine the page configurations
+ */
 let routesConfigs = []
 pages.forEach((pagesConfig) => {
   routesConfigs = routesConfigs.concat(pagesConfig);
 })
 
+/**
+ * Creates a React-Router route for each configured route
+ */
 const dynamicRoutes = routesConfigs.map(routeConfig => {
-
   const component = Loadable({
     loader: routeConfig.loader,
     loading: Spinner
   });
-
   const routeProps = {
     path: routeConfig.path,
     exact: true
   };
-
   const route =
     routeConfig.authenticated ?
       <AuthernticatedRoute {...routeProps} component={component} /> :
       <Route {...routeProps} component={component} />
-
   return route;
 })
 
