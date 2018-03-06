0ab5d109b3b21768afa7eec8fbc419beca0c419d
Daniel Sinnott <sinnott74@hotmail.com>
2018-03-06 15:55:07 +0000

    Adding beforeCreate to check if Tag already exists

--- server/src/Entity/Tag.js
+++ server/src/Entity/Tag.js
@@ -12,4 +12,23 @@ const Tag = ORM.define("tag", {
 
 BlogPost.manyToMany(Tag, { through: "blogposttag" });
 
+/**
+ * Reads a tag with the given name
+ * @param {String} tagName
+ * @return {Promise<Tag>} may resolve to undefined
+ */
+Tag.findByTagName = async function(tagName) {
+  return Tag.findAtMostOne({ name: tagName });
+};
+
+/**
+ * Searches for a Tag with the same Tag name & overwrites this Tag with the persisted Tag.
+ */
+Tag.beforeSave = async function() {
+  const storedTag = await Tag.findByTagName(this.name);
+  if (storedTag) {
+    this.overwrite(storedTag);
+  }
+};
+
 module.exports = Tag;
