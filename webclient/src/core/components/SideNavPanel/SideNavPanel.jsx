ff097d77e09cdd26d871bb25dcf1594c66b216ac
Daniel Sinnott <sinnott74@hotmail.com>
2018-03-12 16:27:13 +0000

    Add personal image & name

--- webclient/src/core/components/SideNavPanel/SideNavPanel.jsx
+++ webclient/src/core/components/SideNavPanel/SideNavPanel.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import SideNavLink from "core/components/SideNavLink";
 import PersonalLinks from "core/components/PersonalLinks";
 import { version } from "../../../../package.json";
+import personalPic from "./me.jpg";
 import "./SideNavPanel.css";
 
 export default class SideNavPanel extends React.Component {
   render() {
     return (
-      <React.Fragment>
+      <div className="sidenvapanel">
         <div className="sidenavpanel__header">
-          <h1 className="sidenavpanel__title">App shell</h1>
+          <img className="sidenavpanel__image" src={personalPic} />
+          <h2 className="sidenavpanel__name">Daniel Sinnott</h2>
+          <h4 className="sidenavpanel__email">Daniel.Sinnott@outlook.com</h4>
         </div>
         <div className="sidenavpanel__body">
           <div className="sidenavpanel__links">
@@ -28,7 +31,7 @@ export default class SideNavPanel extends React.Component {
             <div className="sidenavpanel__version">Version {version}</div>
           </div>
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
