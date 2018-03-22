e3481c7a72fde0c5b0dcdcfc56d9189b5587de84
Daniel Sinnott <sinnott74@hotmail.com>
2018-03-22 23:16:32 +0000

    remove linked list of parents, parent context now becomes child context

--- orm/src/threadlocal.js
+++ orm/src/threadlocal.js
@@ -1,54 +1,23 @@
 const asyncHooks = require("async_hooks");
-const debug = require("debug")("async-local-storage");
 
 const map = new Map();
 
-function isUndefined(value) {
-  return value === undefined;
-}
-
-/**
- * Get data from itself or parent
- * @param {any} data The map data
- * @param {any} key The key
- * @returns {any}
- */
-function recursiveGet(data, key) {
-  if (!data) {
-    return null;
-  }
-  const value = data[key];
-  if (isUndefined(value) && data.parent) {
-    return recursiveGet(data.parent, key);
-  }
-  return value;
-}
-
 const hooks = asyncHooks.createHook({
   init: function init(asyncId, type, triggerAsyncId) {
-    const parentId = triggerAsyncId;
-    // not tigger by itself, add parent
-    if (parentId !== asyncId) {
-      const parent = map.get(parentId);
-      if (parent) {
-        const data = { parent: parent };
-        debug(`${asyncId}(${type}) init by ${triggerAsyncId}`);
-        map.set(asyncId, data);
-        debug(`map size ${map.size}`);
-      }
+    const parentContext = map.get(triggerAsyncId);
+    if (parentContext) {
+      map.set(asyncId, parentContext);
     }
   },
 
   /**
    * Remove the data
    */
-  destroy: function destroy(id) {
-    if (!map.has(id)) {
+  destroy: function destroy(asyncId) {
+    if (!map.has(asyncId)) {
       return;
     }
-    debug(`destroy ${id}`);
-    map.delete(id);
-    debug(`map size ${map.size}`);
+    map.delete(asyncId);
   }
 });
 
@@ -77,11 +46,7 @@ exports.size = () => map.size;
  * @param {String} value The value
  */
 exports.set = function setValue(key, value) {
-  if (key === "parentId") {
-    throw new Error("can't set parentId");
-  }
   const id = getCurrentId();
-  debug(`set ${key}:${value} to ${id}`);
   let data = map.get(id);
   if (!data) {
     data = {};
@@ -97,9 +62,7 @@ exports.set = function setValue(key, value) {
 exports.get = function getValue(key) {
   const id = getCurrentId();
   const data = map.get(id);
-  const value = recursiveGet(data, key);
-  debug(`get ${key}:${value} from ${id}`);
-  return value;
+  return data[key];
 };
 
 /**
