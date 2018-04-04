ce81195641b64a375364add9d417b6ff30207e64
Daniel Sinnott <sinnott74@hotmail.com>
2018-04-04 16:15:26 +0100

    Disable modifing a User

--- server/src/routes/UserRouter.js
+++ server/src/routes/UserRouter.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const User = require("../Entity").User;
-const Credential = require("../Entity").Credential;
 
 router.get("/", async function(req, res) {
   let users = await User.findAll();
@@ -13,24 +12,24 @@ router.get("/:id", async function(req, res) {
   res.json(user);
 });
 
-router.post("/", async function(req, res) {
-  const userData = { ...req.body };
-  const user = new User(userData);
-  await user.save();
-  res.json(user);
-});
+// router.post("/", async function(req, res) {
+//   const userData = { ...req.body };
+//   const user = new User(userData);
+//   await user.save();
+//   res.json(user);
+// });
 
-router.put("/:id", async function(req, res) {
-  const userData = { ...req.body };
-  const user = new User(userData);
-  await user.save();
-  res.json(user);
-});
+// router.put("/:id", async function(req, res) {
+//   const userData = { ...req.body };
+//   const user = new User(userData);
+//   await user.save();
+//   res.json(user);
+// });
 
-router.delete("/:id", async function(req, res) {
-  let id = req.params.id;
-  await User.delete(id);
-  res.sendStatus(200);
-});
+// router.delete("/:id", async function(req, res) {
+//   let id = req.params.id;
+//   await User.delete(id);
+//   res.sendStatus(200);
+// });
 
 module.exports = router;
